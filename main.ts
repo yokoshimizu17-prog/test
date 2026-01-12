bf97e4d38959b5d41b8747b8f7c3424b57ebfc18
yokoshimizu17-prog <yokoshimizu17@gmail.com>
2026-01-12 20:03:31 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,8 @@
-let count = 0
-let running = true
-
-input.onButtonPressed(Button.A, function () {
-    // Aボタンでカウントを止める
-    running = false
-})
-
-input.onButtonPressed(Button.B, function () {
-    // Bボタンでカウントを0にリセットして表示
-    count = 0
-    basic.showNumber(count)
-})
-
-basic.forever(function () {
-    if (running) {
-        count += 1
-        basic.showNumber(count)
-        basic.pause(1000) // 1秒ごとにカウントアップ
-    } else {
-        basic.pause(100) // 停止中は小休止（CPU負荷軽減）
-    }
-})
+/**
+ * カウンターを作る
+ */
+let counter = 0
+// 0からスタート
+counter += 1
+// 1増やす
+basic.showNumber(counter)
