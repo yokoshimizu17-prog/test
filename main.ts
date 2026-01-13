87c9aab9252e202b8cce642acfca6ed0a5452d7a
yokoshimizu17-prog <yokoshimizu17@gmail.com>
2026-01-13 22:14:54 +0900

    Change counter increment from 2 to 3 on button press
    Updated the counter increment logic for button A.

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 input.onButtonPressed(Button.A, function () {
     // 0からスタート
-    counter += 2 // Aボタンを押したとき、数字が2ずつ増えるように変更
+    counter += 3 // Aボタンを押したとき、数字が2ずつ増えるように変更
 })
 /**
  * カウンターを作る
  */
 let counter = 0
 // 0からスタート
-counter += 2
-// 1増やす
+counter += 3
+// 3増やす
 basic.showNumber(counter)
